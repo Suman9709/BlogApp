6d235397f1c861b2b20b2002c1de396006e22514
Suman Kumar <suman.k9709@gmail.com>
2025-04-02 00:54:31 +0530

    renamed server.js

--- frontend1/src/Services/Api.js
+++ frontend1/src/Services/Api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = "http://localhost:5000/api/blogs"
+const API_URL = "https://blogapp-aipn.onrender.com/api/blogs"
 
 export const signUpUser = async (name, username, password) => {
     try {
